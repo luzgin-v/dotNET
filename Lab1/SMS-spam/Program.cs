048a26e93418460aa6a8d037c0ce72fd41be173f
Victor Luzgin <kepuby@inbox.ru>
2016-09-12 11:01:19 +0300

    SMS-spam and Teamwork accepted

---
+++ Lab1/SMS-spam/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace SMS_spam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            int cost = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if ((int)line[i] > 96)
+                {
+                    cost += ((int)line[i] - 97) % 3 + 1;
+                }
+                else if (line[i] == ' ' || line[i] == '.')
+                {
+                    cost++;
+                }
+                else if (line[i] == ',')
+                {
+                    cost += 2;
+                }
+                else if (line[i] == '!')
+                {
+                    cost += 3;
+                }
+            }
+            Console.WriteLine(cost);
+        }
+    }
+}
---
+++ Lab1/Teamwork/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Teamwork
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] numbers = Console.ReadLine().Split(' ');
+            for (int i = 0, sum = 0; i < n; i++)
+            {
+                sum++;
+                if (i + 1 != n && numbers[i] != numbers[i + 1])
+                {
+                    Console.Write(sum + " " + numbers[i] + " ");
+                    sum = 0;
+                }
+                else if (i + 1 == n)
+                {
+                    Console.Write(sum + " " + numbers[i]);
+                }
+            }
+        }
+    }
+}
